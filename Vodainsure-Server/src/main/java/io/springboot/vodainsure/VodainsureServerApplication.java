06bc629e2d5b4c824d32a85d554bff8339ae877d
eliasnjoro1 <kamaelias742@gmail.com>
2023-11-23 14:45:17 +0300

    spring project

---
+++ Vodainsure-Server/src/main/java/io/springboot/vodainsure/VodainsureServerApplication.java
@@ -0,0 +1,13 @@
+package io.springboot.vodainsure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodainsureServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodainsureServerApplication.class, args);
+	}
+
+}
---
+++ Vodainsure-Server/src/test/java/io/springboot/vodainsure/VodainsureServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.vodainsure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VodainsureServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
